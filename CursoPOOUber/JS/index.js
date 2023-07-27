d8df23203374fd070b0e6e60d99cef4ef8faa4f7
DevArv <areventosrd@gmail.com>
2023-07-26 21:39:29 -0400

    classes tipos de uber mas herencias implementadas.

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("LICDEMO01", new Account("Joan Alexander", "DOCDEMO01"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("LICDEMO02", new Account("Nathaly Diaz", "DOCDEMO02"), "HYUNDAI", "I10")
+uberX.passenger = 4;
+uberX.printDataCar();
